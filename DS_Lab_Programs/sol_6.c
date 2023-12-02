be23569302902588a9f9d0411fd60c31bcaab3c2
FoXDev-404 <rajpal010304@gmail.com>
2023-12-02 19:01:01 +0530

    Tower of Hanoi -v2

--- DS_Lab_Programs/sol_6.c
+++ DS_Lab_Programs/sol_6.c
@@ -1,32 +1,37 @@
-// Write a program to implement Tower of Hanoi using recursion.
+// Write a advanced program to implement Tower of Hanoi using recursion and output looks great.
+// Author: Rajpal Nishad, Roll No. 2200950100066
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+#include <math.h>
 
-int opCount = 0;
-
-void TOH(int n, char a, char b, char c)
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
 {
-    if (n > 0)
+    if (n == 1)
     {
-        TOH(n - 1, a, c, b);
-        printf("\n %d : %c -> %c", n, a, c);
-        opCount++;
-        TOH(n - 1, b, a, c);
+        printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
+        return;
     }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
 }
 
 int main()
 {
+    printf("\n====================================\n");
+    printf("Rajpal Nishad, Roll No. 220950100066");
+    printf("\n====================================\n");
+    printf("          Tower of Hanoi");
+    printf("\n====================================\n");
     int n;
-    printf("\n====================================\n");
-    printf("         Tower of Hanoi");
-    printf("\nEnter the number of disks : ");
+    printf("Enter the number of disks: ");
     scanf("%d", &n);
-    printf("\n====================================\n");
-    printf("DISK : OPERATION\n");
-    TOH(n, 'A', 'B', 'C');
-    printf("\n  %d Operation(s) Performed\n", opCount);
+    towerOfHanoi(n, 'A', 'C', 'B');
+    // print how many moves are required
+    printf("\n\n===================================\n");
+    printf("Total number of moves required: %d", (int)(pow(2, n) - 1));
+
+    printf("\n=========-- Thank You! --==========\n");
     return 0;
 }
